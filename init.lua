863b1c1590a8fbe4dad0261deba5f73e55c7a5ca
bleonars <54280259+bleonars@users.noreply.github.com>
2025-08-29 10:03:12 -0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -16,17 +16,38 @@ vim.o.smarttab = true
 vim.o.cinoptions = vim.o.cinoptions .. ":0"
 
 -- util
-vim.o.background = "light"
-vim.o.termguicolors = true
+vim.o.termguicolors = false
+vim.cmd([[colorscheme vim]])
 vim.o.mouse = "a"
-vim.o.clipboard = "unnamedplus"
 vim.o.backspace = "indent,eol,start"
+
+-- clipboard
+vim.o.clipboard = "unnamedplus"
+
+local function paste()
+	return {
+		vim.fn.split(vim.fn.getreg(""), "\n"),
+		vim.fn.getregtype(""),
+	}
+end
+
+vim.g.clipboard = {
+	name = "OSC 52",
+	copy = {
+		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+	},
+	paste = {
+		["+"] = paste,
+		["*"] = paste,
+	},
+}
 
 -- search
 vim.o.incsearch = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
-vim.o.hlsearch = true
+vim.o.hlsearch = false
 
 -- file
 vim.o.number = true
@@ -41,9 +62,9 @@ vim.o.inccommand = "nosplit"
 vim.o.laststatus = 2
 
 -- performance
-vim.o.history = 1000
-vim.o.undofile = true
-vim.o.swapfile = true
+vim.o.history = 10
+vim.o.undofile = false
+vim.o.swapfile = false
 vim.o.lazyredraw = true
 vim.o.redrawtime = 10000
 vim.o.showmode = false
@@ -64,179 +85,129 @@ vim.keymap.set("n", "<leader>bl", ":buffers<CR>", { noremap = true, silent = tru
 vim.keymap.set("n", "<leader>bd", ":bdelete<CR>", { noremap = true, silent = true })
 vim.keymap.set({ "n", "v" }, "<leader>p", '"0p', { noremap = true, silent = true })
 
-local fn = vim.fn
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-	packer_bootstrap =
-		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-end
-
-return require("packer").startup(function(use)
-	use("wbthomason/packer.nvim")
-
-	use({
-		-- NORMAL MODE BASIC
-		-- `gcc` - Toggles the current line using linewise comment
-		-- `gbc` - Toggles the current line using blockwise comment
-		-- `[count]gcc` - Toggles the number of line given as a prefix-count using linewise
-		-- `[count]gbc` - Toggles the number of line given as a prefix-count using blockwise
-		-- `gc[count]{motion}` - (Op-pending) Toggles the region using linewise comment
-		-- `gb[count]{motion}` - (Op-pending) Toggles the region using blockwise comment
-
-		-- VISUAL MODE
-		-- `gc` - Toggles the region using linewise comment
-		-- `gb` - Toggles the region using blockwise comment
-
-		-- NORMAL MODE EXTRA
-		-- `gco` - Insert comment to the next line and enters INSERT mode
-		-- `gcO` - Insert comment to the previous line and enters INSERT mode
-		-- `gcA` - Insert comment to end of the current line and enters INSERT mode
-
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup()
-		end,
-	})
-
-	use({
-		"frazrepo/vim-rainbow",
-		config = function()
-			vim.g.rainbow_active = 1
-		end,
-	})
-
-	-- use({
-	-- 	"shaunsingh/solarized.nvim",
-	-- 	config = function()
-	-- 		require("solarized").set()
-	-- 	end,
-	-- })
-
-	use({
-		"ellisonleao/gruvbox.nvim",
-		config = function()
-			vim.cmd([[colorscheme gruvbox]])
-		end,
-	})
-
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-		config = function()
-			require("lualine").setup({ options = { theme = "auto" } })
-		end,
-	})
-
-	use({
-		"akinsho/toggleterm.nvim",
-		tag = "*",
-		config = function()
-			require("toggleterm").setup({ open_mapping = "<A-t>", direction = "float" })
-		end,
-	})
-
-	use({
-		"mawkler/modicator.nvim",
-		config = function()
-			vim.o.number = true
-			require("modicator").setup()
-		end,
-	})
-
-	use({
-		"karb94/neoscroll.nvim",
-		config = function()
-			require("neoscroll").setup({
-				-- All these keys will be mapped to their corresponding default scrolling animation
-				mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
-				hide_cursor = true, -- Hide cursor while scrolling
-				stop_eof = true, -- Stop at <EOF> when scrolling downwards
-				respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-				cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-				easing_function = nil, -- Default easing function
-				pre_hook = nil, -- Function to run before the scrolling animation starts
-				post_hook = nil, -- Function to run after the scrolling animation ends
-				performance_mode = false, -- Disable "Performance Mode" on all buffers.
-			})
-		end,
-	})
-
-	use({
-		"kdheepak/tabline.nvim",
-		requires = { { "nvim-lualine/lualine.nvim", opt = true }, { "kyazdani42/nvim-web-devicons", opt = true } },
-		config = function()
-			require("tabline").setup()
-			vim.keymap.set("n", "<M-o>", ":TablineBufferNext<CR>", { noremap = true, silent = true })
-			vim.keymap.set("n", "<M-p>", ":TablineBufferPrevious<CR>", { noremap = true, silent = true })
-		end,
-	})
-
-	use({
-		"sbdchd/neoformat",
-		config = function()
-			vim.keymap.set("n", "<leader>f", ":Neoformat<CR>", kb_opts)
-		end,
-	})
-
-	use({
-		"ibhagwan/fzf-lua",
-		requires = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("fzf-lua").setup({
-				fzf_colors = {
-					["bg+"] = { "bg", "CursorLine" },
-					["fg+"] = { "fg", "Normal" },
-					["gutter"] = { "bg", "Normal" },
-				},
-			})
-			vim.keymap.set("n", "<C-p>", "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
-			vim.keymap.set("n", "<C-g>", "<cmd>lua require('fzf-lua').live_grep()<CR>", { silent = true })
-			vim.keymap.set("n", "<C-/>", "<cmd>lua require('fzf-lua').lgrep_curbuf()<CR>", { silent = true })
-		end,
-	})
-
-	use({
-		"tpope/vim-fugitive",
-	})
-
-	use({
-		"mrjones2014/smart-splits.nvim",
-		config = function()
-			vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
-			vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
-			vim.keymap.set("n", "<A-k>", require("smart-splits").resize_up)
-			vim.keymap.set("n", "<A-l>", require("smart-splits").resize_right)
-
-			vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
-			vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
-			vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
-			vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
-
-			vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
-			vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
-			vim.keymap.set("n", "<leader><leader>k", require("smart-splits").swap_buf_up)
-			vim.keymap.set("n", "<leader><leader>l", require("smart-splits").swap_buf_right)
-		end,
-	})
-
-	use({
-		"preservim/tagbar",
-		config = function()
-			vim.keymap.set("n", "<C-t>", ":TagbarToggle<CR>")
-		end,
-	})
-
-	use({
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		requires = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
-	})
-
-	if packer_bootstrap then
-		require("packer").sync()
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
 	end
-end)
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup lazy.nvim
+require("lazy").setup({
+	spec = {
+
+		{
+			"nvim-telescope/telescope.nvim",
+			dependencies = { "nvim-lua/plenary.nvim" },
+			config = function()
+				local builtin = require("telescope.builtin")
+				vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Telescope find files" })
+				vim.keymap.set("n", "<leader>g", builtin.live_grep, { desc = "Telescope live grep" })
+				vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "Telescope buffers" })
+				vim.keymap.set("n", "<leader>h", builtin.help_tags, { desc = "Telescope help tags" })
+			end,
+		},
+
+		{
+			"nvim-neo-tree/neo-tree.nvim",
+			dependencies = {
+				"nvim-lua/plenary.nvim",
+				"MunifTanjim/nui.nvim",
+				"nvim-tree/nvim-web-devicons",
+			},
+		},
+
+		{
+			"sbdchd/neoformat",
+			config = function()
+				vim.keymap.set("n", "<leader>nf", ":Neoformat<CR>", kb_opts)
+			end,
+		},
+
+		{
+			-- NORMAL MODE BASIC
+			-- `gcc` - Toggles the current line using linewise comment
+			-- `gbc` - Toggles the current line using blockwise comment
+			-- `[count]gcc` - Toggles the number of line given as a prefix-count using linewise
+			-- `[count]gbc` - Toggles the number of line given as a prefix-count using blockwise
+			-- `gc[count]{motion}` - (Op-pending) Toggles the region using linewise comment
+			-- `gb[count]{motion}` - (Op-pending) Toggles the region using blockwise comment
+
+			-- VISUAL MODE
+			-- `gc` - Toggles the region using linewise comment
+			-- `gb` - Toggles the region using blockwise comment
+
+			-- NORMAL MODE EXTRA
+			-- `gco` - Insert comment to the next line and enters INSERT mode
+			-- `gcO` - Insert comment to the previous line and enters INSERT mode
+			-- `gcA` - Insert comment to end of the current line and enters INSERT mode
+			"numToStr/Comment.nvim",
+		},
+
+		{
+			"nvim-lualine/lualine.nvim",
+			dependencies = { "nvim-tree/nvim-web-devicons" },
+			config = function()
+				require("lualine").setup()
+			end,
+		},
+
+		{
+			"kdheepak/tabline.nvim",
+			dependencies = { "nvim-lualine/lualine.nvim", "nvim-tree/nvim-web-devicons" },
+			config = function()
+				require("tabline").setup()
+				vim.keymap.set("n", "<M-o>", ":TablineBufferNext<CR>", { noremap = true, silent = true })
+				vim.keymap.set("n", "<M-p>", ":TablineBufferPrevious<CR>", { noremap = true, silent = true })
+			end,
+		},
+
+		{
+			"preservim/tagbar",
+			config = function()
+				vim.keymap.set("n", "<C-t>", ":TagbarToggle<CR>")
+			end,
+		},
+
+		{
+			"mrjones2014/smart-splits.nvim",
+			config = function()
+				vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
+				vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
+				vim.keymap.set("n", "<A-k>", require("smart-splits").resize_up)
+				vim.keymap.set("n", "<A-l>", require("smart-splits").resize_right)
+
+				vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
+				vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
+				vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+				vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
+
+				vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
+				vim.keymap.set("n", "<leader><leader>j", require("smart-splits").swap_buf_down)
+				vim.keymap.set("n", "<leader><leader>k", require("smart-splits").swap_buf_up)
+				vim.keymap.set("n", "<leader><leader>l", require("smart-splits").swap_buf_right)
+			end,
+		},
+
+		{
+			"tpope/vim-fugitive",
+		},
+	},
+
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "vim" } },
+
+	-- automatically check for plugin updates
+	checker = { enabled = true },
+})
